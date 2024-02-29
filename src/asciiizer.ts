c4dc37299105f7add7043de414aed8311e0331d9
JUSTIVE <ben399399@naver.com>
2024-02-29 09:38:19 +0900

    remove ts-belt
         :::::::-::..........
         :::::::::::.........
         :::::::::::..........
         ::::::::::::........:.        .         .         .         .        ..
        .=---::::::-::::.....:....     .                               .  ....:..
        .:--:::::::.:.......... ..                                   :.::-::---:::.
        :::--:::::::::..............                                ::-::.--==-  ..
        :-----:::::::::...........                       .       .:-=---  -**+....
        ------=----::---::::::::.....   .     ...       ......  ..:----*+:+**=-.
        -----------:::::::::.......                     . .---:...=:...+++*+=:.
        =-------------::-:::::.....                     ...:::....::::::::......
       .===------==--------:::::::..                   .......::..:..:::.... ..
       :=======----=-----:-:::.....                    . ........:..............
       :=----------------::-:::....                   .....:.             .:-:..
       --             -------=--..        *@%%*-.     ...-:-              ::::..
       ==             -=-------::..     +@@@@@@@@=        ...  ..................
       =====----------------::::...   -@@@@@@@@@@@%       ......::::::::............
      .++================---:::::..  -@@@@@@@@@@@@@*       ....::::::::::::.......::
      .++++++++++===+===----:::::.. .@@@@@@@@@@@@@@@.      ...:::::-:::::.......::::
      :++++++++++++=====-----::::.. +@@@@@@@@@@@@@@@=       ....::-::::::.....:::::-
      :+++++++++++++===--::..::.... %@@@@@@@@@@%*=@@#           .--::         .:---=
        .  .+*++++++=         :...  @@@@@@@@@#*#%*@@%        ....:::::::::.:::-=----
      =++++++++++======----:::::..::#@@@@@%@@%#%#*+@*        ....:::::::::::---:----
      =================------::::....@@#%@@@@**#@@*=.        .....:::::::::--:-:===-
      =============-=-=---::::...    @@*#%@@#*++#*-:.        .      .:::--::::-.==-:
      .  ------...  .:::::::         @@*++###+:=+*==*      ....:::-=*#-.*++@@@=.--:.
      :--::------::::::....:--.      %@**###%+-=#*==*-         .:=*##*=@@@@@@@@.:::.
     .==-:.   .::::::.   ......      *@*#%%@@@@@@#=+#=...    .:-+=:=+=:#%@@@@@@*....
     -#@#:.:           --::-.  . .   *%*#%@%@@@#**++%+.::      :-...-::=+#@@@@@@
     :#%=  .           +=%###%%%%    #@%*##*+**+=++#@+         +-+-       @@@@@@:--=
     .*#:  ---:::    ##*+    ####    *@@####@@@@@+=@@:       :+%@@*   #%@@@@@@@@::..
      **  .==+*=-.  :#**=.   *+#+    -@@@#%%@@@@#**@@.:: -@* ..:+*=   %@@@@@@@@@.
     =********+*+++++======--=-*:-- ::%@@@@@@@%**#@@*::::-+--..-++=   @@%@@@@@@@.
     +#*******++++++====----:=*+-+= +=@@@@@@%##**@@@=-::--+==..:--=   %@@@@@@@@@@#=
     +#******+++++=====----:.-%==+*-=@@@@@@@@@@@@@@@@:  --+=@.:::=+     @@@@@@%#*+=.
     =#******+++++====----::#%@@@@@@@@@@@@@@@@@@@@@@@@@@%@@@@@# -:+    :@@@@@  .:-=:
     -#*****++++=====---:::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*      @@@@@@ *%%%@+
     -****===--:::..    .:#@@@@@@@@@@@@@@@@@@@@%@@@@@@@@@@@@@@@@@  :**@@@@@@@ +####+
    .-+***              .@@@@@@@@@@@@@@@@@@%%%**@@@@@@@@@@@@@@@@@    :%@@@@@@ =**###
    .-+##+              #@@@@@@@@@@@@@@@@@@@--=@@@@@@@@@@@@@@@@@@*    =@@@@@@ =***##
    :--               *@@@@@@@@@@@@@@@@@@@@@%@@@@@@@@@@@@@@@@@@@@@:    @@@@@@ -#*###
    -=+               .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#@=..:-*@@@@@.-#*###
    +=+@@@@#*++==-----%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@==%%@@@@@@@:-####%
    *+*-:*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*==+#@@:-%%%@@
    #+*++@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#-%@@-:%%%%+

--- src/asciiizer.ts
+++ src/asciiizer.ts
@@ -1,27 +1,26 @@
-import { AR, R, pipe } from "@mobily/ts-belt";
-import Jimp from "jimp";
-import fs from "fs";
-import { IMAGE_NAME } from "./picture";
+import Jimp from 'jimp'
+import fs from 'fs'
+import { IMAGE_NAME } from './picture'
 
 // ASCII 문자 설정
-const ASCII_CHARS = "@%#*+=-:. ";
+const ASCII_CHARS = '@%#*+=-:. '
 const clamp = (value: number, min: number, max: number) =>
-  Math.min(Math.max(value, min), max);
+  Math.min(Math.max(value, min), max)
 
 const imageToAscii = async (path: string): Promise<string> => {
   try {
-    const image = await Jimp.read(path);
+    const image = await Jimp.read(path)
     const buffer = await image
       .resize(80, (80 * image.bitmap.height) / image.bitmap.width) // 크기 조정
       .greyscale() // 그레이스케일로 변환
       .contrast(0.5) // 대비 향상
-      .getBufferAsync(Jimp.MIME_JPEG);
-    let asciiImage = "";
+      .getBufferAsync(Jimp.MIME_JPEG)
+    let asciiImage = ''
 
     for (let y = 0; y < image.bitmap.height; y++) {
       for (let x = 0; x < image.bitmap.width; x++) {
-        const idx = image.getPixelIndex(x, y);
-        const grayValue = image.bitmap.data[idx];
+        const idx = image.getPixelIndex(x, y)
+        const grayValue = image.bitmap.data[idx]
         const char =
           ASCII_CHARS[
             clamp(
@@ -29,23 +28,23 @@ const imageToAscii = async (path: string): Promise<string> => {
               0,
               ASCII_CHARS.length - 1
             )
-          ] ?? ASCII_CHARS[ASCII_CHARS.length - 1];
-        asciiImage += char;
+          ] ?? ASCII_CHARS[ASCII_CHARS.length - 1]
+        asciiImage += char
       }
-      asciiImage += "\n";
+      asciiImage += '\n'
     }
 
-    return "\n" + asciiImage;
+    return '\n' + asciiImage
   } catch (err) {
-    return "";
+    return ''
   }
-};
+}
 
-const ASCII_FILE = "picomi.txt";
+const ASCII_FILE = 'picomi.txt'
 const writeAsciiToFile = async (ascii: string) => {
-  await fs.writeFileSync(ASCII_FILE, ascii);
-};
+  await fs.writeFileSync(ASCII_FILE, ascii)
+}
 
 export const genAscii = async () => {
-  pipe(await imageToAscii(`${IMAGE_NAME}.jpg`), writeAsciiToFile);
-};
+  writeAsciiToFile(await imageToAscii(`${IMAGE_NAME}.jpg`))
+}
--- src/promise.ts
+++
@@ -1,9 +0,0 @@
-export const map =
-  <T, U>(fn: (x: T) => U) =>
-  (x: Promise<T>): Promise<U> =>
-    x.then(fn);
-
-export const flatMap =
-  <T, U>(fn: (x: T) => Promise<U>) =>
-  (x: Promise<T>): Promise<U> =>
-    x.then(fn);
